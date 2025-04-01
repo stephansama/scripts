88cfb40ace65e74350f8c2494f304318c1b00b03
stephansama <stephanrandle@hotmail.com>
2025-03-31 21:39:14 -0400

    chore: created monorepo package json search script

---
+++ fzf-pkgjson.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+origin=$(git rev-parse --show-toplevel)
+
+query=$(
+	find "$origin" -type f -name 'package.json' \
+		-not -path '*node_modules*' \
+		-not -path '*.sst*' \
+		-not -path '*.next*' \
+		-not -path '*.vitepress*' | sed -e "s@$origin@.@g" -e "s@package.json@@g" | fzf | sed -e "s@^.@$origin@"
+)
+
+cd "$query" || return
