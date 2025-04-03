5ad11c13159a3d5524088d6c29fd3ae79baf882c
stephansama <stephanrandle@hotmail.com>
2025-04-02 23:04:43 -0400

    chore: updated package json script to cache results

--- fzf-pkgjson.sh
+++ fzf-pkgjson.sh
@@ -2,12 +2,22 @@
 
 origin=$(git rev-parse --show-toplevel)
 
-query=$(
+historyFile="$origin/node_modules/.package-json.history"
+
+if [[ ! -f $historyFile ]]; then
 	find "$origin" -type f -name 'package.json' \
 		-not -path '*node_modules*' \
+		-not -path '*.open-next*' \
 		-not -path '*.next*' \
 		-not -path '*.sst*' \
-		-not -path '*.vitepress*' |
+		-not -path '*.vitepress*' >>"$historyFile"
+	echo
+fi
+
+current=$(cat "$historyFile")
+
+query=$(
+	echo "$current" |
 		sed -e "s@$origin@.@g" -e "s@package.json@@g" |
 		fzf |
 		sed -e "s@^.@$origin@"
