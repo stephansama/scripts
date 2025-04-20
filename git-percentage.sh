dfb6ddb4dc2ac6dbac767edb938dcb42176f00c5
stephansama <stephanrandle@hotmail.com>
2025-04-20 09:05:58 -0400

    chore: update percentage script

--- git-percentage.sh
+++ git-percentage.sh
@@ -4,4 +4,7 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 
 output=$(git diff ORIG_HEAD "$branch" --shortstat)
 
-echo "$output"
+echo "$output" | while IFS=, read -r _ additions deletions; do
+	echo "$additions"
+	echo "$deletions"
+done | sed 's/[^0-9]*//g' | paste -sd- - | bc
